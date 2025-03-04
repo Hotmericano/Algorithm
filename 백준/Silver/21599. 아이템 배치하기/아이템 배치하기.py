68995854044edac08bd13bc78e2473c08088698e
Hotmericano <110606231+Hotmericano@users.noreply.github.com>
2025-03-04 15:57:36 +0900

    [Silver II] Title: 아이템 배치하기, Time: 456 ms, Memory: 80688 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/21599. 아이템 배치하기/아이템 배치하기.py
+++ 백준/Silver/21599. 아이템 배치하기/아이템 배치하기.py
@@ -1,11 +1,12 @@
 n =  int(input())
-ef_table = [0 for i in range(n)]
+
 ef_power = list(map(int, input().split()))
 
 ef_power = list(reversed(sorted(ef_power)))
-
+answer = 0
 for i in range(n):
-    for j in range(ef_power[i]):
-        ef_table[(i+j+1)%n] = 1
-        
-print(ef_table.count(1))
+    if ef_power[i] == 0:
+        break
+    answer = max(answer, i+ef_power[i]) # i+인 이유: 전거는 무조건 강화되어 있음
+
+print(min(n,answer))
