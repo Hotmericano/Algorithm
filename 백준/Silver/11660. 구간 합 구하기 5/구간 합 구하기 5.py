aca161bf160d14e709aeb27f8c44e452053e0d83
Hotmericano <110606231+Hotmericano@users.noreply.github.com>
2025-03-26 20:07:32 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1192 ms, Memory: 107396 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -9,13 +9,14 @@ for i in range(H):
 
 sum = [[0 for _ in range(H+1)] for _ in range(H+1)]
 
-for i in range(1, H +1):
+for i in range(1,H+1):
+    for j in range(1,H+1):
+        sum[i][j] = sum[i][j-1] + square[i-1][j-1]
+
+for i in range(1,H+1):
     for j in range(1, H+1):
-        sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + square[i-1][j-1]
+        sum[j][i] = sum[j-1][i] + sum[j][i]
 
 for i in range(Q):
     l,r,L,R = map(int, input().split())
     print(sum[L][R] + sum[l-1][r-1] - sum[l-1][R] - sum[L][r-1])
-
-
-    
